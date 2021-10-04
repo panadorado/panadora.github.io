7a3ebfffaa9de14750fee1e8a638b0d848894fb5
Panadora <chenhuanhui123@gmail.com>
2021-10-04 23:31:12 +0700

    Update Poste 2021-10-4

---
+++ _posts/2021-10-4-komorebi-animated-wallpapers-for-Linux.md
@@ -0,0 +1,80 @@
+---
+thumbnail: komorebi-animated-wallpapers-for-Linux.png
+layout: post
+title: Komorebi - Animated Wallpapers cho Linux OS
+excerpt: Komorebi là một trình quản lý hình nền động tuyệt vời cho tất cả các nền tảng Linux.
+author: Pandora
+category: [linux]
+permalink: /komorebi-animated-wallpapers-for-Linux.html
+---
+
+![image](../image/komorebi-animated-wallpapers-for-Linux.png)
+<br><br>
+**Để có thể một hình nền động tuyệt đẹp trên desktop chạy hệ điều hành Windows của [Microssorft](https://www.microsoft.com/vi-vn) thì chúng ta phải có:** 
+
+* [Wallpapper Engine](https://store.steampowered.com/app/431960/Wallpaper_Engine/) : Chú ý để có thể trải nghiệm tốt các tình năng có trên bộ cộng cụ này buộc các bạn phải trả phí mua trên Steam, hoặc bản có thể tải phiên bản crack [tại đây](https://linkneverdie.net/software/wallpaper-engine-6879).
+
+* [Lively Wallpaper](https://www.microsoft.com/en-us/p/lively-wallpaper/9ntm2qc6qws7?activetab=pivot:overviewtab) : Bộ công cụ này được miễn phí trên Store của microssoft chạy trên Windows 10, nên các pc hay laptop nào đang sử dụng Windows 10 trở lên đều có thể cài được.
+
+* [Push Video Wallpapper](https://taimienphi.vn/download-push-video-wallpaper-32846) : Tương tự như 2 bộ công cụ trên đó là dùng để làm ảnh động bằng video cho Desktop và hoàn toàn miễn phí.
+
+Điều đặc biệt trên đó là tất cả phần mềm trên đều được hổ trợ trên Windows, vậy để có thể dùng video làm ảnh động trên các hệ điều hành khác chẳn hạn như HĐH Linux thì sao... ?. Tất nhiên là có thì mình mới viết bài này đăng cho các bạn xem chứ 😂 hihihi, luyên thuyên vậy đủ rồi mình vào chủ đề chính nhen, vậy nó là phần mềm **[Komorebi](https://github.com/cheesecakeufo/komorebi)**.<br><br>
+
+### 1. Komorebi là gì ?
+<hr>
+
+**Komorebi** là một trình quản lý hình nền động tuyệt vời cho tất cả các nền tảng Linux. Nó cung cấp hình nền hình ảnh, video và trang web có thể tùy chỉnh hoàn toàn có thể được tinh chỉnh bất cứ lúc nào!
+
+![image-1](../image/komorebi-animated-wallpapers-for-Linux-1.png)
+
+### 2. Cài đặt và sử dụng Komorebi.
+<hr>
+
+Mở **`Terminal`** lên bằng cách <kbd>Ctrl + Alt + t</kbd> và nhập lệnh sau:
+
+**Cài đặt các gói cần thiết**
+
+```console
+
+ ~$ sudo add-apt-repository ppa:gnome3-team/gnome3 -y
+ ~$ sudo add-apt-repository ppa:vala-team -y
+ ~$ sudo add-apt-repository ppa:gnome3-team/gnome3-staging -y
+ ~$ sudo apt install cmake valac libgtk-3-dev libgee-0.8-dev libclutter-gtk-1.0-dev libclutter-1.0-dev libwebkit2gtk-4.0-dev libclutter-gst-3.0-dev
+
+```
+
+**Cài đặt komorebi**
+
+```console
+
+ ~$ git clone https://github.com/cheesecakeufo/komorebi.git
+ ~$ cd komorebi
+ ~$ mkdir build && cd build
+ ~$ cmake .. && sudo make install && ./komorebi
+
+```
+
+### 3. Custom video làm ảnh nền cho desktop
+<hr>
+Komorebi cung cấp một công cụ đơn giản để tạo hình nền của riêng bạn! Đơn giản, mở ứng dụng của bạn và tìm kiếm 'Trình tạo hình nền'.<br><br>
+
+![image-2](../image/komorebi-animated-wallpapers-for-Linux-2.png)<br><br>
+![image-3](../image/komorebi-animated-wallpapers-for-Linux-3.png)<br><br>
+![image-4](../image/komorebi-animated-wallpapers-for-Linux-4.png)<br><br>
+Bạn có thể sử dụng hình ảnh, video hoặc trang web làm hình nền và bạn có nhiều tùy chọn khác nhau để tùy chỉnh hình nền của riêng mình!
+
+### 5. Gỡ bỏ komorebi.
+<hr>
+
+```console
+
+ // Nếu bạn đã cài đặt phiên bản đóng gói của Komorebi
+ ~$ sudo apt remove komorebi
+ 
+ // Nếu bạn cài đặt thủ công Komorebi
+ ~$ cd komorebi/build
+ ~$ sudo make uninstall
+
+```
+
+Lời kết: Như vậy bạn đó có 1 hình nền desktop động tuyệt đẹp riêng cho chính mình, Bạn nào đã sử dụng rồi thì cho mình ý kiến nhá, và còn bộ công cụ nào hổ trợ làm ảnh động desktop cho linux nữa hay không thì các bạn chỉa sẽ cho mình và người khác cùng biết nhá.
--- assets/search.js
+++ assets/search.js
@@ -1,42 +1,63 @@
 (function() {
   function displaySearchResults(results, store) {
     var searchResults = document.getElementById('search-results');
-    //var errorpage = document,getElementById('')
+    // var errorpage = document.getElementById('');
 
-    if (results.length) { // Are there any results?
+    if (results.length) { 
+      
+      // Are there any results?
       var appendString = '';
 
-      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+      for (var i = 0; i < results.length; i++) {  
+        
+        // Iterate over the results
         var item = store[results[i].ref];
         appendString += 
-        '<div class="col-md-4">' + 
-          '<div class="single-blog">' + 
-            '<br>' +
-            '<small class="row">' +
-              '<span class="col-sm"><i class="fas fa-book-reader"> Đăng bởi: '+ item.author +'</i></span>' +
-              '<span class="col-sm"><i class="fas fa-calendar-alt"> Ngày đăng: '+ item.date +'</i></span>' +
-            '</small><br>' +
-            '<a href="' + item.url + '">' +
-              '<img id="img-thumbnail" class="img-thumbnail img-hover" src="' + item.site + '/image/' + item.thumbnail + '" style="height: 180px;" title="' + item.url + '">' +
-            '</a><br>' +
-            '<a class="entry-title" href="' + item.url + '" title="' + item.url + '">' + item.title + '</a><br>' +
-            '<p>' + item.content.substring(0, 60) + ' ...</p>' +
-            '<div class="row">' +
-              '<div class="col-sm-8"><a class="fb-like" data-href="' + item.site + '/' + item.url + '" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></a></div>' +
-              '<div class="col-sm"><a class="read-more-btn text-center" href=' + item.url + '>Đọc thêm</a></div>' +
-            '</div><br>' +
-          '</div>' +
-        '</div>';
+        `<div class="col-md-4">
+          <div class="single-blog">
+            <br>
+            <small class="row">
+              <span class="col-sm"><i class="fas fa-book-reader"> Đăng bởi: ${item.author}</i></span>
+              <span class="col-sm"><i class="fas fa-calendar-alt"> Ngày đăng: ${item.date}</i></span>
+            </small><br>
+            <a href="${item.url}">
+              <img id="img-thumbnail" class="img-thumbnail img-hover" src="${item.site}/image/${item.thumbnail}" style="height: 180px;" title="${item.url}">
+            </a><br>
+            <a class="entry-title" href="${item.url}" title="${item.url}">${item.title}</a><br>
+            <p> ${item.content.substring(0, 60)} ...</p>
+            <div class="row">
+              <div class="col-sm-8"><a class="fb-like" data-href="${item.site}/${item.url}" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></a></div>
+              <div class="col-sm"><a class="read-more-btn text-center" href="${item.url}" >Đọc thêm</a></div>
+            </div><br>
+          </div>
+        </div>`;
       }
 
       searchResults.innerHTML = appendString;
     } 
     else 
     {
-      searchResults.innerHTML = '<div class="container post bg-light container-border-radius py-3 px-4 my-lg-4 my-3 post-content" style="border-radius: 20px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);">' +
-                                '<div class="col-lg-12 col-md-9 col-sm-12">' +
-                                '<center><p>Không tìm thấy kết quả tìm kiếm của bạn !</p></center>' + 
-                                '<center><img src="../image/404pagenotfound.jpeg" width="250"></center></div></div>';
+      searchResults.innerHTML = 
+      `<div class="container post bg-light container-border-radius py-3 px-4 my-lg-4 my-3 post-content searchResult">
+        <center>
+          <div class="page-notfound">
+            <svg width="250px" height="400px" viewBox="0 0 837 1045" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                <path d="M353,9 L626.664028,170 L626.664028,487 L353,642 L79.3359724,487 L79.3359724,170 L353,9 Z" id="Polygon-1" stroke="#007FB2" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                <path d="M78.5,529 L147,569.186414 L147,648.311216 L78.5,687 L10,648.311216 L10,569.186414 L78.5,529 Z" id="Polygon-2" stroke="#EF4A5B" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                <path d="M773,186 L827,217.538705 L827,279.636651 L773,310 L719,279.636651 L719,217.538705 L773,186 Z" id="Polygon-3" stroke="#795D9C" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                <path d="M639,529 L773,607.846761 L773,763.091627 L639,839 L505,763.091627 L505,607.846761 L639,529 Z" id="Polygon-4" stroke="#F2773F" stroke-width="6" sketch:type="MSShapeGroup"></path>
+                <path d="M281,801 L383,861.025276 L383,979.21169 L281,1037 L179,979.21169 L179,861.025276 L281,801 Z" id="Polygon-5" stroke="#36B455" stroke-width="6" sketch:type="MSShapeGroup"></path>
+              </g>
+            </svg>
+            <div class="message-box">
+                <h1>404<small> Not found</small></h1>
+                <p>Không tìm thấy thông tin !</p>
+            </div>
+          </div>
+        </center> 
+      </div>
+      `;
     }
   }
 
@@ -44,13 +65,17 @@
     var query = window.location.search.substring(1);
     var vars = query.split('&');
 
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
-
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+    if(query.trim().length > 6)
+    {
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+  
+        if (pair[0] === variable) {
+          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        }
       }
     }
+    else window.location.replace("/");
   }
 
   var searchTerm = getQueryVariable('query');
@@ -62,7 +87,6 @@
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
       this.field('id');
-
       this.field('title', { boost: 10 });
       this.field('author');
       this.field('category');
